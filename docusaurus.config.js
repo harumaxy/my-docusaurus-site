1c13dfffd7e6b11b9fb4fbee73e33834d5f5c495
max <harumaxy@gmail.com>
2020-11-15 12:35:28 +0900

    udpate edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,13 +83,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/harumaxy/my-docusaurus-site/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/harumaxy/my-docusaurus-site/tree/master/blog',
+            'https://github.com/harumaxy/my-docusaurus-site/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
